de9b33ab2847b6f67af4d4bf328b3fe8b344e22d
agu1lera <r.aguilera314@gmail.com>
2025-10-25 22:55:50 -0700

    implemented logWalk function and getTopWalker

--- Hood.cpp
+++ Hood.cpp
@@ -6,17 +6,60 @@ Hood::Hood()
 {
 	cout << "Creating a default neighborhood object" << endl;
 }
-
+//--------------------------------------------------------------
 Hood::Hood(string cityName) : WalkData(cityName)
 {
 	cout << "Creating neighborhood object" << endl;
 }
-
+//--------------------------------------------------------------
 LinkedList& Hood::getList()
 {
 	return hoodList;
 }
+//--------------------------------------------------------------
+//logs walked miles
+void Hood::logWalk(const string& personName, int miles)
+{
+	Node* current = hoodList.getFirst();
+	while (current)
+	{
+		if (current->x->getName() == personName)
+		{
+			current->x->setTotalMiles(current->x->getTotalMiles() + miles);
+			totalMiles += miles;
+			return;
+		}
+		current = current->getNext();
+	}
 
+	//add person
+	WalkData* newPerson = new WalkData(personName, miles);
+	hoodList.addNode(newPerson);
+	totalMiles += miles;
+}
+//--------------------------------------------------------------
+//finds person with the highest miles
+WalkData* Hood::getTopWalker() const
+{
+	Node* current = hoodList.getFirst();
+	if (!current)
+		return nullptr;
+
+	Node* topWalker = current;
+	current = current->getNext();
+
+	while (current)
+	{
+		if (current->x->getTotalMiles() > topWalker->x->getTotalMiles())
+		{
+			topWalker = current;
+		}
+		current = current->getNext();
+	}
+
+	return topWalker->x;
+}
+//--------------------------------------------------------------
 void Hood::printHoodList() const
 {
 	cout << "\n===The City of " << name << "'s Walking Data===" << endl;
@@ -31,12 +74,12 @@ void Hood::printHoodList() const
 		i++;
 	}
 }
-
+//--------------------------------------------------------------
 void Hood::writeToStream(ostream& out) const
 {
 	out << "Location: " << getName() << " Total Miles Logged: " << getTotalMiles() << endl;
 }
-
+//--------------------------------------------------------------
 ostream& operator<<(ostream& out, const Hood& city)
 {
 	city.writeToStream(out);
