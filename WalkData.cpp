1f881c6682b9187d8c762f9bd9c8f636876879a1
agu1lera <r.aguilera314@gmail.com>
2025-10-25 22:57:58 -0700

    added //--- spacers for visual purposes

--- WalkData.cpp
+++ WalkData.cpp
@@ -1,51 +1,52 @@
 #include "WalkData.h"
 #include <iostream>
+
 WalkData::WalkData()
 {
 	name = "No name";
 	totalMiles = 0;
 	cout << "Creating walking activity log..." << endl;
 }
-
+//----------------------------------------------------------
 WalkData::WalkData(string itemName)
 {
 	name = itemName;
 	totalMiles = 0;
 	cout << "Creating walking activity log..." << endl;
 }
-
+//----------------------------------------------------------
 WalkData::WalkData(string itemName, int itemMiles)
 {
 	name = itemName;
 	totalMiles = itemMiles;
 	cout << "Creating walking activity log..." << endl;
 }
-
+//----------------------------------------------------------
 string WalkData::getName() const
 {
 	return name;
 }
-
+//----------------------------------------------------------
 int WalkData::getTotalMiles() const
 {
 	return totalMiles;
 }
-
+//----------------------------------------------------------
 void WalkData::setName(string newName)
 {
 	name = newName;
 }
-
+//----------------------------------------------------------
 void WalkData::setTotalMiles(int miles)
 {
 	totalMiles = miles;
 }
-
+//----------------------------------------------------------
 void WalkData::writeToStream(ostream& out) const
 {
 	out << "Name: " << getName() << ", Miles: " << getTotalMiles() << endl;
 }
-
+//----------------------------------------------------------
 ostream& operator<<(ostream& out, WalkData* log)
 {
 	log->writeToStream(out);
