69286ea67e02757e05bd9c997c7e7d9e41ceb3d9
agu1lera <r.aguilera314@gmail.com>
2025-10-25 19:47:27 -0700

    Implmented removeNode(), clear() and implemented the Big 5

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -27,28 +27,68 @@ Node* LinkedList::getLast() const
 //--------------------------------------------------
 LinkedList::LinkedList(const LinkedList& other)
 {
+	first = nullptr;
+	last = nullptr;
+	noe = 0;
+	
+	Node* current = other.first;
+	while (current != nullptr)
+	{
+		WalkData* copyData = new WalkData(*current->getItem());
+		addNode(copyData);
+		current = current->getNext();
+	}	
 }
 //--------------------------------------------------
-/*
 LinkedList& LinkedList::operator=(const LinkedList& RHS)
 {
+	if (this != &RHS)
+	{
+		//call clear function
+		clear();
 
-}*/
+		Node* current = RHS.first;
+		while (current != nullptr)
+		{
+			addNode(new WalkData(*current->getItem()));
+			current = current->getNext();
+		}
+	}
+	return *this;
+}
 //--------------------------------------------------
 LinkedList::~LinkedList()
 {
+	clear();
 }
 //--------------------------------------------------
 LinkedList::LinkedList(LinkedList&& RHS)
 {
+	first = RHS.first;
+	last = RHS.last;
+	noe = RHS.noe;
+
+	RHS.first = nullptr;
+	RHS.last = nullptr;
+	RHS.noe = 0;
 }
 //--------------------------------------------------
-/*
 LinkedList& LinkedList::operator=(LinkedList&& RHS)
 {
+	if (this != &RHS)
+	{
+		clear();
 
-}*/
+		first = RHS.first;
+		last = RHS.last;
+		noe = RHS.noe;
 
+		RHS.first = nullptr;
+		RHS.last = nullptr;
+		RHS.noe = 0;
+	}
+	return *this;
+}
 //--------------------------------------------------
 void LinkedList::addNode(WalkData* data)
 {
@@ -78,5 +118,68 @@ void LinkedList::addNode(WalkData* data)
 		}
 		else cout << "Memory could not be allocated" << endl;
 	}
+}
+//--------------------------------------------------
+bool LinkedList::removeNode(string name)
+{
+	if (noe == 0 || first == nullptr)
+		return false;
+
+	Node* current = first;
+
+	while (current)
+	{
+		WalkData* data = current->getItem();
+		if (data && data->getName() == name)
+		{
+			Node* prev = current->getPrevious();
+			Node* next = current->getNext();
+
+			if (current == first)
+			{
+				first = next;
+				if (first)
+					first->setPrevious(nullptr);
+			}
+			else
+			{
+				if (prev)
+					prev->setNext(next);
+			}
+
+			if (current == last)
+			{
+				last = prev;
+				if (last)
+					last->setNext(nullptr);
+			}
+			else
+			{
+				if (next)
+					next->setPrevious(prev);
+			}
+			delete current;
+			noe--;
+			return true;
+		}
+
+		current = current->getNext();
+	}
+
+	return false;
+}
+//--------------------------------------------------
+void LinkedList::clear()
+{
+	Node* current = first;
+	while (current)
+	{
+		Node* next = current->getNext();
+		delete current;
+		current = next;
+	}
+	first = nullptr;
+	last = nullptr;
+	noe = 0;
 }
 
