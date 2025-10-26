cf1e45f5f06b7cc969c86a0552569a6bfc01683a
agu1lera <r.aguilera314@gmail.com>
2025-10-25 20:54:16 -0700

    updated walk function to check city in person's list, if city/hood not found then add city/hood

--- Person.cpp
+++ Person.cpp
@@ -7,24 +7,43 @@ Person::Person()
 {
 	cout << "Creating person object..." << endl;
 }
-
+//-----------------------------------------------------------
 Person::Person(string personName) : WalkData(personName)
 {
 	cout << "Creating person object..." << endl;
 }
-
+//-----------------------------------------------------------
 void Person::walk(WalkData* city, int miles)
 {
-	totalMiles += miles;
-	WalkData* log = new WalkData(name, miles);
-	if (Hood* c = dynamic_cast<Hood*>(city))
-	{
-		personList.addNode(new WalkData(c->getName(), miles));
-		c->getList().addNode(log);
-		c->setTotalMiles(c->getTotalMiles() + miles);
-	}
-}
+    totalMiles += miles;
+    string cityName = city->getName();
 
+    // check if the city already exists in person's list
+    Node* current = personList.getFirst();
+    while (current)
+    {
+        if (current->x->getName() == cityName)
+        {
+            current->x->setTotalMiles(current->x->getTotalMiles() + miles);
+            break;
+        }
+        current = current->getNext();
+    }
+
+    // add new city if not found
+    if (!current)
+    {
+        WalkData* newCity = new WalkData(cityName, miles);
+        personList.addNode(newCity);
+    }
+
+    // update Hood data using logWalk()
+    if (Hood* c = dynamic_cast<Hood*>(city))
+    {
+        c->logWalk(name, miles);
+    }
+}
+//-----------------------------------------------------------
 void Person::printPersonList() const
 {
 	cout << "\n===" << name << "'s Walking Data===" << endl;
@@ -37,14 +56,12 @@ void Person::printPersonList() const
 		i++;
 	}
 }
-
-
+//-----------------------------------------------------------
 void Person::writeToStream(ostream& out) const
 {
 	out << "Username: " << getName() << ", Total miles walked: " << getTotalMiles() << endl;
 }
-
-
+//-----------------------------------------------------------
 /*Prints person's name and total stats*/
 ostream& operator<<(ostream& out, const Person* human)
 {
